01498f11137af476054568411b1d38d4443624a9
Divya37 <Divya37@missionrnd.com>
2016-03-31 01:09:11 +0500

    Submit Commit

--- src/FixBST.cpp
+++ src/FixBST.cpp
@@ -32,6 +32,45 @@ struct node{
 	struct node *right;
 };
 
-void fix_bst(struct node *root){
+void swap(int* a, int* b)
+{
+	int t = *a;
+	*a = *b;
+	*b = t;
+}
 
+void path(struct node* root, struct node** first,
+                    struct node** middle, struct node** last,
+                    struct node** prev)
+{
+	if (root!=NULL)
+	{
+		path(root->left, first, middle, last, prev);		
+		if (*prev && root->data < (*prev)->data)
+		{
+			
+			if (!*first)
+			{
+				*first = *prev;
+				*middle = root;
+			}			
+			else
+				*last = root;
+		}		
+		*prev = root;		
+		path(root->right, first, middle, last, prev);
+	}
+}
+
+void fix_bst(struct node *root)
+{
+	struct node *first, *middle, *last, *prev;
+	first = middle = last = prev = NULL;
+
+	path(root, &first, &middle, &last, &prev);
+
+	if (first && last)
+		swap(&(first->data), &(last->data));
+	else if (first && middle)
+		swap(&(first->data), &(middle->data));
 }
